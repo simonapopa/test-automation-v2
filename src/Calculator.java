a07e4d31b16337abdd69a5a2f164c5baf8eb37e7
Simona <popa_simona_bh@yahoo.com>
2018-11-13 16:46:39 +0200

    changes in Main.class

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,80 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class Calculator {
 
     // move all operational methods here
+
+
+    // adunare
+    public static long adunare(long firstNumber, long secondNumber) {
+        long result = firstNumber + secondNumber;
+        return result;
+    }
+
+    //scadere
+    public static long scadere(long firstNumber, long secondNumber) {
+        long result = firstNumber - secondNumber;
+        return result;
+    }
+
+    //inmultire
+    public static long inmultire(long firstNumber, long secondNumber) {
+        long result = firstNumber * secondNumber;
+        return result;
+    }
+
+    //impartire
+    public static float impartire(float firstNumber, float secondNumber) {
+        float result = firstNumber / secondNumber;
+        return result;
+    }
+
+    public static void secondExercise() {
+        Scanner scanner = new Scanner(in);
+
+        System.out.print("define a byte value:");
+        byte myByte = scanner.nextByte();
+
+        System.out.print("define a short value:");
+        short myShort = scanner.nextShort();
+
+        System.out.print("define an int value:");
+        int myInt = scanner.nextInt();
+
+        System.out.print("define a long value:");
+        long myLong = scanner.nextLong();
+
+        System.out.print("define a float value:");
+        float myFloat = scanner.nextFloat();
+
+        System.out.print("define a double value:");
+        double myDouble = scanner.nextDouble();
+        boolean myBoolean = true;
+        String myString = "test";
+
+
+        //long result = myInt + myLong;
+
+        System.out.println(myString + (myByte + myShort + myInt + myLong + myFloat + myDouble) + myBoolean);
+        System.out.println(myString + (myByte - myShort - myInt - myLong - myFloat - myDouble) + myBoolean);
+        System.out.println(myString + (myByte * myShort * myInt * myLong * myFloat * myDouble) + myBoolean);
+        System.out.println(myString + (myByte / myShort / myInt / myLong / myFloat / myDouble) + myBoolean);
+    }
+
+    // 3. Write a Java program to divide two numbers and print on the screen.
+    public static void thirdExercise(long first, long second) {
+        System.out.println("Sum is = " + (first + second));
+    }
+
+    // 5. Write a Java program that takes two numbers as input and display the product of two numbers.
+    public static void fifthExercise(long first, long second) {
+        System.out.print(first + "x" + second + "=" + first * second);
+    }
+
+    // 8. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
+    public static float seventhExercise(long first, long second, long third) {
+        return (float) (first + second + third) / (3);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 import static java.lang.System.in;
 
-public class Main {
+public class Main extends Calculator{
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(in);
@@ -52,108 +52,9 @@ public class Main {
         System.out.println("Distance " + distance + " miles as speed is " + ((distance / 1000.0f) / (eleventhExercise(h, m, s) / 3600.0f)) / 1.609f + " miles/seconds");
     }
 
-
-    // adunare
-    public static long adunare(long firstNumber, long secondNumber) {
-        long result = firstNumber + secondNumber;
-        return result;
-    }
-
-    //scadere
-    public static long scadere(long firstNumber, long secondNumber) {
-        long result = firstNumber - secondNumber;
-        return result;
-    }
-
-    //inmultire
-    public static long inmultire(long firstNumber, long secondNumber) {
-        long result = firstNumber * secondNumber;
-        return result;
-    }
-
-    //impartire
-    public static float impartire(float firstNumber, float secondNumber) {
-        float result = firstNumber / secondNumber;
-        return result;
-    }
-
-    private static void firstTraining() {
-        //System.out.println("Hello world!");
-
-        Scanner scanner = new Scanner(in);
-
-        // 1st exercise: print sum of two numbers
-        /*
-        byte myByte = scanner.nextByte();
-        short myShort = scanner.nextShort();
-        int myInt = scanner.nextInt();
-        long myLong = scanner.nextLong();
-        float myFloat = scanner.nextFloat();
-        double myDouble = scanner.nextDouble();
-        boolean myBoolean = true;
-        String myString = "test";
-        */
-        int celsius = scanner.nextInt();
-        float finalTemp;
-        finalTemp = (float) 5 / 9 * (celsius - 32);
-
-        System.out.println("Temperature is " + finalTemp);
-
-
-        //long result = myInt + myLong;
-    /*
-        System.out.println(myString + (myByte + myShort + myInt + myLong + myFloat + myDouble) + myBoolean);
-        System.out.println(myString + (myByte - myShort - myInt - myLong - myFloat - myDouble) + myBoolean);
-        System.out.println(myString + (myByte * myShort * myInt * myLong * myFloat * myDouble) + myBoolean);
-        System.out.println(myString + (myByte / myShort / myInt / myLong / myFloat / myDouble) + myBoolean);
-*/
-        int x = 2;
-        x += x += x++ + ++x;
-        System.out.println("x =" + x);
-
-
-    }
-
     // 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line
     public static String firstExercise(String name) {
         return name;
-    }
-
-    public static void secondExercise() {
-        Scanner scanner = new Scanner(in);
-
-        System.out.print("define a byte value:");
-        byte myByte = scanner.nextByte();
-
-        System.out.print("define a short value:");
-        short myShort = scanner.nextShort();
-
-        System.out.print("define an int value:");
-        int myInt = scanner.nextInt();
-
-        System.out.print("define a long value:");
-        long myLong = scanner.nextLong();
-
-        System.out.print("define a float value:");
-        float myFloat = scanner.nextFloat();
-
-        System.out.print("define a double value:");
-        double myDouble = scanner.nextDouble();
-        boolean myBoolean = true;
-        String myString = "test";
-
-
-        //long result = myInt + myLong;
-
-        System.out.println(myString + (myByte + myShort + myInt + myLong + myFloat + myDouble) + myBoolean);
-        System.out.println(myString + (myByte - myShort - myInt - myLong - myFloat - myDouble) + myBoolean);
-        System.out.println(myString + (myByte * myShort * myInt * myLong * myFloat * myDouble) + myBoolean);
-        System.out.println(myString + (myByte / myShort / myInt / myLong / myFloat / myDouble) + myBoolean);
-    }
-
-    // 3. Write a Java program to divide two numbers and print on the screen.
-    public static void thirdExercise(long first, long second) {
-        System.out.println("Sum is = " + (first + second));
     }
 
     /* 4. Write a Java program to print the result of the following operations:
@@ -195,22 +96,12 @@ public class Main {
 
     }
 
-    // 5. Write a Java program that takes two numbers as input and display the product of two numbers.
-    public static void fifthExercise(long first, long second) {
-        System.out.print(first + "x" + second + "=" + first * second);
-    }
-
     // 6. Write a Java program to display the following pattern
     public static void sixthExercise() {
         System.out.println("   J    a   v     v  a ");
         System.out.println("   J   a a   v   v  a a");
         System.out.println("J  J  aaaaa   V V  aaaaa");
         System.out.println(" JJ  a     a   V  a     a");
-    }
-
-    // 8. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
-    public static float seventhExercise(long first, long second, long third) {
-        return (float) (first + second + third) / (3);
     }
 
     // 9. Write a Java program to display the following pattern

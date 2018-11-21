a6e708789f83a9a659206607259053883e6850b5
Simona <popa_simona_bh@yahoo.com>
2018-11-21 17:29:42 +0200

    1-6 array exercises

--- src/ArraysListsExercises/FifthSessionMain.java
+++ src/ArraysListsExercises/FifthSessionMain.java
@@ -1,7 +1,5 @@
 package ArraysListsExercises;
 
-import ArraysListsExercises.CollectionsMethods;
-
 import java.util.Scanner;
 
 public class FifthSessionMain {
@@ -18,18 +16,37 @@ public class FifthSessionMain {
         }
 
         //Array exercise: 2.Write a Java program to calculate the average value of array elements.
-        double average = coll.getAverageFromArray(array);
+        //double average = coll.getAverageFromArray(array);
         //System.out.println(average);
 
         // Array exercise: 3.Write a Java program to test if an array contains a specific value.
-        System.out.println("Please enter number: ");
-        int value = scanner.nextInt();
-        boolean valoare = coll.getSpecificValue(array, value);
-        System.out.println(valoare);
+        //System.out.print("Please enter a number: ");
+        //int value = scanner.nextInt();
+        // boolean valoare = coll.getSpecificValue(array, value);
+        // System.out.println(valoare);
 
         //Array exercise: 4.Write a Java program to find the index of an array element.
+       /* System.out.print("Please enter a number: ");
+        int value = scanner.nextInt();
+        int index = coll.getIndexOfArray(value);
+        int index = coll.getIndexOfArrayTwo(array, value);
+        System.out.println("Index of " + value + " is " + index);*/
 
+        //Array exercise: 5.Write a Java program to remove a specific element from an array.
+        /*System.out.println("Array:");
+        for ( int i = 0; i < array.length; i++ ) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("\n Give number to remove: ");
+        int value = scanner.nextInt();
+        int[] arrayElement = coll.removeElementFromArray(value);
+        for ( int i = 0; i < arrayElement.length; i++ ) {
+            System.out.print(arrayElement[i] + " ");
+        }*/
 
-
+        //Array exercise: 6.Write a Java program to find the second smallest element in an array.
+       // int smallest = coll.getSecondSmallestElementFromArray();
+        //int smallest = coll.getSecondSmallestElementFromArrayTwo();
+        coll.getSecondSmallestElementFromArray();
     }
 }

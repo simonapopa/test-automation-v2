2b61f2b5ed0a1709f3f29011650f07148c86581b
Lenovo <IonutRares.Bucur@endava.com>
2018-12-02 23:21:11 +0200

    hash map exercises

--- src/HashMapsExercises/HashMapMain.java
+++ src/HashMapsExercises/HashMapMain.java
@@ -15,9 +15,34 @@ public class HashMapMain {
         HashMap<Integer, String> myhMap = hm.getMaxFromKey(value);
         System.out.println(myhMap);
 
-        // Write a Java program to iterate through all elements in a hash list
-        hm.getIteratedList(myhMap);
+        // Hash maps exercise: Write a Java program to iterate through all elements in a hash list
+        //hm.getIteratedList(myhMap);
 
+        // Hash maps exercise: Write a Java program to get the number of elements in a hash set
+        System.out.print("Number of elements in hash map is " + hm.getNumberOfElementsHashMap(myhMap));
+        System.out.println();
 
+        // Hash maps exercise: Write a Java program to empty an hash set
+        //myhMap.clear();
+        //System.out.println("Emptied map " + myhMap);
+
+        // Hash maps exercise: Write a Java program to test a hash set is empty or not
+        System.out.println("Map is empty? " + myhMap.isEmpty());
+
+        // Hash maps exercise: Write a Java program to clone a hash set to another hash set
+        //HashMap<Integer, String> newHashMap = (HashMap<Integer, String>) hm.cloneAndGetHashSet(myhMap);
+        //System.out.println("Cloned map is " + newHashMap);
+
+        // Hash maps exercise: Write a Java program to compare two hash set
+//        System.out.print("Size of second list: ");
+//        value = scanner.nextInt();
+//        HashMap<Integer, String> myNewHashMap = hm.getMaxFromKey(value);
+//        System.out.println(myNewHashMap);
+//        hm.compareTwoHashSet(myhMap, myNewHashMap);
+
+        //Hash maps exercise: Write a Java program to remove all of the elements from a hash set
+        hm.removeAllElements(myhMap);
+        System.out.println(myhMap);
+        System.out.println("Map is empty? " + myhMap.isEmpty());
     }
 }

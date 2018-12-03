38a7ea964adc977046b09cc56b6cc8346b91e006
Lenovo <IonutRares.Bucur@endava.com>
2018-12-03 13:00:37 +0200

    l

--- src/HashMapsExercises/HashMapMain.java
+++ src/HashMapsExercises/HashMapMain.java
@@ -34,15 +34,15 @@ public class HashMapMain {
         //System.out.println("Cloned map is " + newHashMap);
 
         // Hash maps exercise: Write a Java program to compare two hash set
-//        System.out.print("Size of second list: ");
-//        value = scanner.nextInt();
-//        HashMap<Integer, String> myNewHashMap = hm.getMaxFromKey(value);
-//        System.out.println(myNewHashMap);
-//        hm.compareTwoHashSet(myhMap, myNewHashMap);
+        System.out.print("Size of second list: ");
+        value = scanner.nextInt();
+        HashMap<Integer, String> myNewHashMap = hm.getMaxFromKey(value);
+        System.out.println(myNewHashMap);
+        hm.compareTwoHashSet(myhMap, myNewHashMap);
 
         //Hash maps exercise: Write a Java program to remove all of the elements from a hash set
-        hm.removeAllElements(myhMap);
-        System.out.println(myhMap);
-        System.out.println("Map is empty? " + myhMap.isEmpty());
+//        hm.removeAllElements(myhMap);
+//        System.out.println(myhMap);
+//        System.out.println("Map is empty? " + myhMap.isEmpty());
     }
 }

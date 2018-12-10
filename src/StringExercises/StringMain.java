4f70682d19d07cef2b04593b9c301b4da9cff90f
Simona <popa_simona_bh@yahoo.com>
2018-12-10 15:53:52 +0200

    party of the String homework

---
+++ src/StringExercises/StringMain.java
@@ -0,0 +1,16 @@
+package StringExercises;
+
+import ArraysListsExercises.ReadNumbersFromKeyboard;
+
+public class StringMain {
+    public static void main(String args[]) {
+        ReadNumbersFromKeyboard read = new ReadNumbersFromKeyboard();
+        StringMethods sm = new StringMethods();
+
+        //sm.readStringBuilder(sm.appendStringBuilder(sm.generateEmptyStringBuilder()));
+        sm.readStringBuilder(sm.insertNewString(sm.appendStringBuilder(sm.generateEmptyStringBuilder())));
+        sm.generateEmptyStringBuilder();
+        sm.readStringBuilder(sm.appendStringBuilder(sm.generateEmptyStringBuilder()));
+
+    }
+}
---
+++ src/StringExercises/StringMethods.java
@@ -0,0 +1,43 @@
+package StringExercises;
+
+import ArraysListsExercises.ReadNumbersFromKeyboard;
+
+public class StringMethods {
+    ReadNumbersFromKeyboard read = new ReadNumbersFromKeyboard();
+
+    //String exercise: 1. Create an empty StringBuffer
+    public StringBuilder generateEmptyStringBuilder() {
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder;
+    }
+
+    //String exercise: print string builder
+    public void readStringBuilder(StringBuilder stringBuilder) {
+        System.out.println("String builder is: " + stringBuilder);
+    }
+
+    //String exercise: 2. Append “blessings”
+    public StringBuilder appendStringBuilder(StringBuilder stringBuilder) {
+        System.out.println("Give what to append (eg. 'blessings')");
+        String string = read.readStringFromKey();
+        stringBuilder.append(string);
+        return stringBuilder;
+    }
+
+    //String exercise: 3. Insert “Good ” in the begging
+    public StringBuilder insertNewString(StringBuilder stringBuilder) {
+        System.out.println("Give what to insert (eg. 'Good ')");
+        String string = read.readStringFromKey();
+        int offset = 0;
+        stringBuilder.insert(offset, string);
+        return stringBuilder;
+    }
+
+    //String exercise: 5. Append “ be with you”
+    public StringBuilder appendMultipleStrings(StringBuilder stringBuilder){
+        System.out.println("Give what to append (eg. ' be with you'");
+        String string = read.readStringFromKey();
+
+        return stringBuilder;
+    }
+}

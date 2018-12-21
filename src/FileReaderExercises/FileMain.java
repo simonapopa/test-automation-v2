4cddf1567847b92fbf6190dddf0895cc8c739ba9
Simona <popa_simona_bh@yahoo.com>
2018-12-21 16:14:48 +0200

    file exercises

--- src/FileReaderExercises/FileMain.java
+++ src/FileReaderExercises/FileMain.java
@@ -1,4 +1,13 @@
 package FileReaderExercises;
 
+import java.io.IOException;
+
 public class FileMain {
+
+    public static void main(String args[]) throws IOException {
+        FileMethods fm = new FileMethods();
+        //fm.createFile();
+        //fm.checkFileExists(fm.createFile());
+        fm.writeInFile(fm.createFile());
+    }
 }

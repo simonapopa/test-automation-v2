7373a48af738c211796f11845e4fb133403cdf81
simona.bucur <simona.bucur@smartbox.com>
2023-12-14 12:35:15 +0200

    - update gitingore file - update Excluded folders from future commits

--- src/test/java/com/pages/AbstractPage.java
+++
@@ -1,9 +0,0 @@
-package com.pages;
-
-import net.serenitybdd.core.pages.PageObject;
-
-public abstract class AbstractPage extends PageObject {
-    public void windowMaximize() {
-        getDriver().manage().window().maximize();
-    }
-}
